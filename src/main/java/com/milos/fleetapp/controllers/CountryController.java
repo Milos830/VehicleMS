f656049561a75cd7daa910502eb2dad378f5bb48
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-03-18 09:39:44 +0100

    added list of contries logic to service, controller and html page. Display list of countries

--- src/main/java/com/milos/fleetapp/controllers/CountryController.java
+++ src/main/java/com/milos/fleetapp/controllers/CountryController.java
@@ -1,13 +1,30 @@
 package com.milos.fleetapp.controllers;
 
+import com.milos.fleetapp.models.Country;
+import com.milos.fleetapp.services.CountryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
     @GetMapping("/countries")
-    public String getCountry() {
+    public String getCountry(Model model) {
+
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries", countryList);
+
         return "country";
     }
+
+
 }
--- src/main/java/com/milos/fleetapp/services/CountryService.java
+++ src/main/java/com/milos/fleetapp/services/CountryService.java
@@ -1,7 +1,21 @@
 package com.milos.fleetapp.services;
 
+import com.milos.fleetapp.models.Country;
+import com.milos.fleetapp.repositories.CountryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public CountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+        // this method get list of all the countries
+    public List<Country> getCountries () {
+        return countryRepository.findAll();
+    }
 }
